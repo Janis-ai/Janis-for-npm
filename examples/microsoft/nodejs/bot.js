239d9511716b48effd42d566fc2f7807590d55c2
Michael Nathanson <michael.nathanson@gmail.com>
2019-02-08 19:54:51 -0500

    Adding azure v4 example

---
+++ examples/microsoft/nodejs/bot.js
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+const path = require('path');
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+const { ActivityTypes } = require('botbuilder');
+
+
+
+
+
+
+class MyBot {
+    /**
+     *
+     * @param {ConversationState} conversation state object
+     */
+    constructor(conversationState, janis) {
+        // Creates a new state accessor property.
+        // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors
+        this.conversationState = conversationState;
+        this.janis = janis;
+    }
+
+    normalize(obj) {
+        if (obj.conversation && obj.conversation.id) {
+            obj.channel = obj.conversation.id;
+            obj.client_key = process.env.JANIS_CLIENT_KEY;
+            if (obj.recipient && obj.recipient.id) {
+                obj.user = obj.recipient.id;
+            }
+        }
+    }
+
+    structureReply(src, resp) {
+        var msg = {};
+        if (typeof(resp) == 'string') {
+            msg.text = resp;
+        } else {
+            msg = resp;
+        }
+        msg.channelData = src.channelData;
+        msg.channel = src.channel;
+        msg.serviceUrl = src.serviceUrl;
+        msg.conversation = src.conversation;
+        msg.from = src.recipient;
+        msg.recipient = src.from;
+        return msg;
+    };
+    
+    async processChatResponse(turnContext, message) {
+        message.type = "message"
+        await turnContext.sendActivity(`${ message.text }`);        
+        await this.janis.hopOut(message)
+    }
+
+    async respond(isPaused, message, elements, turnContext) {
+        if (isPaused) {
+            return Promise.resolve(false)
+        };
+        for (var i = 0; i < elements.length; i++) {
+            var obj = elements[i]
+            obj = this.structureReply(message, obj);
+
+            obj.channel = message.channel;
+            obj.metadata = {
+                "bot": "janis"
+            };
+            if (obj.quick_replies) {
+                obj.metadata.quick_replies = obj.quick_replies;
+            }
+            await turnContext.sendActivity(`${ obj.text }`);
+            await this.janis.hopOut(obj)
+        }
+    }
+
+    /**
+     *
+     * Use onTurn to handle an incoming activity, received from a user, process it, and reply as needed
+     *
+     * @param {TurnContext} on turn context object.
+     */
+    async onTurn(turnContext, message) {
+        // Handle message activity type. User's responses via text or speech or card interactions flow back to the bot as Message activity.
+        // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+        // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            this.normalize(message)
+            let isBotPaused = await this.janis.hopIn(message)
+            // If your bot is paused, stop it from replying
+            if (isBotPaused) { return };
+            if (message.text == "help") {
+                // send a Janis alert to your slack channel
+                // that the user could use assistance                      
+                await this.janis.assistanceRequested(message);
+            }
+            //array of responses
+            var responses = ["yeah!"]
+            await this.respond(isBotPaused, message, responses, turnContext);          
+        } else {
+            // Generic handler for all other activity types.
+            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+        }
+        // Save state changes
+        await this.conversationState.saveChanges(turnContext);
+    }
+}
+
+
+
+exports.MyBot = MyBot;
--- examples/microsoft/nodejs/index.js
+++ examples/microsoft/nodejs/index.js
@@ -1,87 +1,140 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-require('dotenv').config();
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-var janis = require('janis');
-var apiKey = process.env.JANIS_API_KEY; // <= key provided by janis for Slack
-var clientKey = process.env.JANIS_CLIENT_KEY; // <= key provided by janis for Slack
-var botPlatform = 'microsoft'; // <= possible values: 'messenger', 'slack', 'microsoft'
-var token = process.env.MESSENGER_PAGE_ACCESS_TOKEN; // <= to see profile image in transcript for Messenger channel, you must include
-var janis = janis(apiKey, clientKey, {platform: botPlatform, token:token});
+// Import required packages
+const path = require('path');
+const restify = require('restify');
 
-//=========================================================
-// Bot Setup
-//=========================================================
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter, ConversationState, MemoryStorage, ActivityTypes, TurnContext } = require('botbuilder');
+const { MicrosoftAppCredentials } = require('botframework-connector');
+// Import required bot configuration.
+const { BotConfiguration } = require('botframework-config');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/botframework/receive', connector.listen());
+const { MyBot } = require('./bot');
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+// Read botFilePath and botFileSecret from .env file
+// Note: Ensure you have a .env file and include botFilePath and botFileSecret.
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+// Get the .bot file path
+// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
+let botConfig;
+try {
+    // Read bot configuration from .bot file.
+    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
+} catch (err) {
+    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
+    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
+    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
+    console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
+    process.exit();
+}
+
+const Janis = require('janis');
+var janisApiKey = process.env.JANIS_API_KEY; // <= key provided by JANIS for Slack
+var janisClientKey = process.env.JANIS_CLIENT_KEY; // <= key provided by JANIS for Slack
+var janis = Janis(janisApiKey, janisClientKey, {useWebhook: false});
+
 
 // Handle forwarding the messages sent by a human through your bot
-janis.on('chat response', function (message) {
-    // Send notification as a proactive message
-    sendIt(message, message.text);
-});
-
-var sendIt = function(session, text) {
-    var msg = new builder.Message()
-        .address(session.address)
-        .text(text);
-    msg.data.metadata = session.metadata;
-    bot.send(msg, function (err) {
-      if (err) {
-        return;
-      };
-      janis.hopOut(msg.data);
+let chatResponseHandler = function(message) {
+    // Send notification as a proactive message 
+    var activity = message.activity
+    var reference = TurnContext.getConversationReference(activity); 
+    MicrosoftAppCredentials.trustServiceUrl(activity.serviceUrl);
+    
+    adapter.continueConversation(reference, async (context) => {
+        // Complete the job.
+        await bot.processChatResponse(context, message);
     });
 }
 
-var intents = new builder.IntentDialog();
-bot.dialog('/', intents);
-
-intents.matches(/^hi/i, function (session) {
-    console.log(JSON.stringify(session.message, null, 2))
-    janis.hopIn(session.message, function(isBotPaused) {
-      // If your bot is paused, stop it from replying
-      if (isBotPaused) { return };
-      sendIt(session.message, "Hello there.");
-    });
+janis.on('chat response', function (message) {
+    chatResponseHandler(message)
 });
 
-intents.matches(/^help/i, function (session) {
-    janis.hopIn(session.message, function(isBotPaused) {
-      // If your bot is paused, stop it from replying
-      if (isBotPaused) { return };
-      // let the user know that they are being routed to a human                     
-      sendIt(session.message, "Hang tight. Let me see what I can do.");
-      // send a janis alert to your slack channel
-      // that the user could use assistance                      
-      janis.assistanceRequested(session.message);
-    });
+// For local development configuration as defined in .bot file
+const DEV_ENVIRONMENT = 'development';
+
+// Define name of the endpoint configuration section from the .bot file
+const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
+
+// Get bot endpoint configuration by service name
+// Bot configuration as defined in .bot file
+const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
+
+// Create bot adapter.
+// See https://aka.ms/about-bot-adapter to learn more about bot adapter.
+const adapter = new BotFrameworkAdapter({
+    appId: endpointConfig.appId || process.env.microsoftAppID,
+    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
 });
 
-intents.onDefault(function (session, args, next) {
-    janis.hopIn(session.message, function(isBotPaused) {
-      // If your bot is paused, stop it from replying
-      if (isBotPaused) { return };
-      // let the user know that the bot does not understand
-      sendIt(session.message, "Huh?");
-      // capture conversational dead-ends.
-      janis.logUnkownIntent(session.message);
-    });
-    next();
+// Catch-all for any unhandled errors in your bot.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    console.error(`\n [onTurnError]: ${ error }`);
+    // Send a message to the user
+    await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await conversationState.delete(context);
+};
+
+// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a state store to persist the dialog and user state between messages.
+let conversationState;
+
+// For local development, in-memory storage is used.
+// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
+// is restarted, anything stored in memory will be gone.
+const memoryStorage = new MemoryStorage();
+conversationState = new ConversationState(memoryStorage);
+
+// CAUTION: You must ensure your product environment has the NODE_ENV set
+//          to use the Azure Blob storage or Azure Cosmos DB providers.
+// const { BlobStorage } = require('botbuilder-azure');
+// Storage configuration name or ID from .bot file
+// const STORAGE_CONFIGURATION_ID = '<STORAGE-NAME-OR-ID-FROM-BOT-FILE>';
+// // Default container name
+// const DEFAULT_BOT_CONTAINER = '<DEFAULT-CONTAINER>';
+// // Get service configuration
+// const blobStorageConfig = botConfig.findServiceByNameOrId(STORAGE_CONFIGURATION_ID);
+// const blobStorage = new BlobStorage({
+//     containerName: (blobStorageConfig.container || DEFAULT_BOT_CONTAINER),
+//     storageAccountOrConnectionString: blobStorageConfig.connectionString,
+// });
+// conversationState = new ConversationState(blobStorage);
+
+// Create the main dialog.
+const bot = new MyBot(conversationState, janis);
+
+let webhookHandler = function(req, res) {
+    var obj = req.body;    
+    if (req.headers['message-type'] == "channel update") {
+        return;
+    } else if (req.headers['message-type'] == "chat response") {
+        chatResponseHandler(obj)
+        return;
+    } else {
+        adapter.processActivity(req, res, async (context) => {
+            // route to main dialog.
+            await bot.onTurn(context, req.body);
+        });
+    }
+};
+
+// Create HTTP server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open myBot-with-counter.bot file in the Emulator`);
+});
+
+// Listen for incoming activities and route them to your bot main dialog.
+server.post('/api/messages', (req, res) => {
+    webhookHandler(req, res)
 });
